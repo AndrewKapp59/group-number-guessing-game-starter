d31976dc8fa75fb1d88deba8688076d4f664551a
Andrew Kapp <dkapp59@gmail.com>
2022-02-03 14:45:10 -0600

    server returns array the records guesses to client. Client apends the array to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,18 +3,37 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!")
   $('#button-submit').on('click', addGuesses)
+  // startNewGame();
+
 }
+
+// <Game Start> ---------------------------------------
+// function startNewGame(){
+//   $.ajax({
+//     method: 'GET',
+//     url:'/startNewGame'
+//   })
+// };
+// </Game Start> ---------------------------------------
+
 
 
 function addGuesses() {
+  console.log('inside addGuesses');
+  
   let brantInput = $('#input-brant-guess').val();
   let drewInput = $('#input-drew-guess').val();
   let markInput = $('#input-mark-guess').val();
   let joshInput = $('#input-josh-guess').val();
+
+  
   $('#input-brant-guess').val('');
   $('#input-drew-guess').val('');
   $('#input-mark-guess').val('');
   $('#input-josh-guess').val('');
+  console.log('inputs emptied');
+  
+
   // use AJAX to make a post request to the server
   $.ajax({
     method: 'POST', 
@@ -29,9 +48,40 @@ function addGuesses() {
       }
     }
   }).then(function(response){
-    console.log('Item Added');
-    getInventory(); // to refresh the DOM with the new item
+    console.log('Item Added', response);
+    getScoreboard(); // to refresh the DOM with the new item
   }).catch(function(response){
     console.log('UGHHHH addItem not Working');
   })
+}
+
+
+function getScoreboard () {
+  console.log('inside getScoreboard');
+
+  // gets inventory array from the server using AJAX
+  $.ajax({
+    method: 'GET',
+    url: '/scoreboard'
+  }).then(function(response){
+    console.log('Scoreboard appended', response);
+    // TODO append quotes to DOM
+    renderToDom(response);
+  }).catch(function(response){
+    console.log('getScoreboard not working', response);
+  });
+}
+
+function renderToDom(scoreboard){
+  // empty element before re-appending everything
+  $('#scoreboard').empty();
+  for (let round of scoreboard){
+      $('#scoreboard').append(`<tr>
+      <td>${round.round}</td>
+      <td>${round.brant}</td>
+      <td>${round.drew}</td>
+      <td>${round.mark}</td>
+      <td>${round.josh}</td>
+      </tr>`)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser')
 const app = express();
 const PORT = 5000;
 
+let correctNumber;
+
+let scoreboard = [];
+
+
 // This must be added before GET & POST routes.
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -10,26 +15,50 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static('server/public'));
 
 
+// GET & POST Routes go here
+//gets the inputs from the client 
 app.post('/guesses', function(req, res){
   // the data that is send from the client is saved for us in req.body
-  // without body-parser set up, req.body is undefined 
+  // i.e. the data object is = req.body
   console.log('req.body from the POST is', req.body);
-
-  // // grabs the new item from the request body
-  // let item = req.body.itemToAdd;
-
-  // inventoryList.push(item);
-
-  // // send back a status code of 201
-  // res.sendStatus(201);
+  scoreboard.push(req.body.itemToGuess);
+  res.send(201);
 })
 
+app.get('/scoreboard', function(req, res){
+  console.log('request at /scoreboard was made', req.body);
+  res.send(scoreboard);
+});
 
 
 
+// Now we need to test those elements against the current correctNumber value
+// function checkGuess (guess) {
+//   if( guess === correctNumber) {
+    
+//     return guess; 
+
+//   }
+//   else if( guess > correctNumber) {
+
+//   }
+//   else if( guess < correctNumber) {
+
+//   }
+// }
 
 
-// GET & POST Routes go here
+// produces a random guess and assigns it to correctNumber
+function randomNumberGen() {
+  correctNumber = Math.floor(Math.random() * ((25-1) + 1 ));
+  console.log('Current correct guess', correctNumber);
+}
+
+// When the server turns on, the random number is created and stored on the server.
+randomNumberGen();
+
+
+
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
