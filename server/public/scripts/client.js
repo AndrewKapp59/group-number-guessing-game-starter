aad0808cf3a9bc0be619449f616a25a24f50abf3
Andrew Kapp <dkapp59@gmail.com>
2022-02-03 11:45:11 -0600

    Able to send search inputs to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,36 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  $('#button-submit').on('click', addGuesses)
+}
+
+
+function addGuesses() {
+  let brantInput = $('#input-brant-guess').val();
+  let drewInput = $('#input-drew-guess').val();
+  let markInput = $('#input-mark-guess').val();
+  let joshInput = $('#input-josh-guess').val();
+  $('#input-brant-guess').val('');
+  $('#input-drew-guess').val('');
+  $('#input-mark-guess').val('');
+  $('#input-josh-guess').val('');
+  // use AJAX to make a post request to the server
+  $.ajax({
+    method: 'POST', 
+    url: '/guesses',
+    data: {
+      itemToGuess: {
+        round: '',
+        brant: brantInput,
+        drew: drewInput,
+        mark: markInput,
+        josh: joshInput,
+      }
+    }
+  }).then(function(response){
+    console.log('Item Added');
+    getInventory(); // to refresh the DOM with the new item
+  }).catch(function(response){
+    console.log('UGHHHH addItem not Working');
+  })
 }
--- server/server.js
+++ server/server.js
@@ -9,8 +9,27 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
-// GET & POST Routes go here
 
+app.post('/guesses', function(req, res){
+  // the data that is send from the client is saved for us in req.body
+  // without body-parser set up, req.body is undefined 
+  console.log('req.body from the POST is', req.body);
+
+  // // grabs the new item from the request body
+  // let item = req.body.itemToAdd;
+
+  // inventoryList.push(item);
+
+  // // send back a status code of 201
+  // res.sendStatus(201);
+})
+
+
+
+
+
+
+// GET & POST Routes go here
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
